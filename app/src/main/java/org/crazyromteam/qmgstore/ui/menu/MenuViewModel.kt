947a81413d20537bceba5217b4eca561e597e0b0
erikiorai <erikiorai@hotmail.com>
2026-02-23 22:30:03 +0200

    Removed unnecessary text from MenuViewModel class

--- app/src/main/java/org/crazyromteam/qmgstore/ui/menu/MenuViewModel.kt
+++ app/src/main/java/org/crazyromteam/qmgstore/ui/menu/MenuViewModel.kt
@@ -1,13 +1,6 @@
 package org.crazyromteam.qmgstore.ui.menu
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MenuViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
-    }
-    val text: LiveData<String> = _text
 }
